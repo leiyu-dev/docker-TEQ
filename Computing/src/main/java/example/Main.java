17d69f68f5f6ebcb084faf5295332d5208673b43
lei_yu <1799726703@qq.com>
2024-10-15 20:32:17 +0800

    init

--- Computing/src/main/java/example/Main.java
+++ Computing/src/main/java/example/Main.java
@@ -20,7 +20,7 @@ class mynode extends AbstractFlinkNode{
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Simulator simulator = new Simulator();
         simulator.addNode(mynode.class);
         simulator.start();
--- Computing/src/main/java/org/teq/simulator/Simulator.java
+++ Computing/src/main/java/org/teq/simulator/Simulator.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import javassist.*;
 
 public class Simulator {
     public List<Class<AbstractFlinkNode>> nodes = new ArrayList<>();
@@ -20,53 +21,34 @@ public class Simulator {
             System.out.println("Class "+ clazz.getName() +" is not a subclass of AbstractFlinkNode");
         }
     }
-    public void start(){
+    public void start() throws Exception {
         System.out.println("Simulator started");
         for(Class<?>clazz : nodes){
-            System.out.println("Methods of "+ clazz.getName() +" class:");
-            Method[] methods = clazz.getMethods();
-            for (Method method : methods) {
-                System.out.println(method.getName());
-            }
-            saveClassToFile(clazz);
-            System.out.println("==================\n");
+            // 使用Javassist加载这个匿名类
+            ClassPool pool = ClassPool.getDefault();
+            CtClass ctClass = pool.get(clazz.getName());
+
+            // 修改类的名字为 'example.mynode'
+            ctClass.setName("example.mynode");
+
+            // 将修改后的字节码写入文件
+            byte[] byteCode = ctClass.toBytecode();
+            saveClassToFile(byteCode, "docker/example/mynode.class");
+
+            System.out.println("Class saved as 'docker/example/mynode.class'");
         }
     }
-    static int tot_node=0;
-    public void saveClassToFile(Class<?> anonymousClass){
-        // 获取类的类加载器
-        ClassLoader classLoader = anonymousClass.getClassLoader();
-
-        // 生成类文件的路径
-        String classFilePath = anonymousClass.getName().replace('.', '/') + ".class";
-
-        // 从类加载器中获取.class文件的字节码
-        InputStream classStream = classLoader.getResourceAsStream(classFilePath);
-
-        if (classStream == null) {
-            System.out.println("Class file not found for: " + classFilePath);
-            return;
+    private static void saveClassToFile(byte[] classData, String filePath) throws IOException {
+        File file = new File(filePath);
+        File parentDir = file.getParentFile();
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs();  // 创建父目录
         }
-
-        // 将字节码保存到一个独立的.class文件
-        try {
-            File outputFile = new File("node"+(tot_node++) + ".class");
-            try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = ((InputStream) classStream).read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-                System.out.println("Saved class file to: " + outputFile.getAbsolutePath());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                classStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        try (FileOutputStream fos = new FileOutputStream(filePath)) {
+            fos.write(classData);
         }
     }
 }
